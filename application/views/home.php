81476e31b9632e7f08c368ba1dd0b8ff3a68eab0
noufalck <noufalck15@gmail.com>
2016-01-24 16:06:47 +0530

    our home

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,155 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>collage</title>
+<html>
+<head><title>webpage></title>
+<style>
+#container
+{
+width:100%;
+height:17%;
+background:#FFE4C4;
+}
+#header
+{
+width:20%;
+height:10%;
+background:#FFE4C4;
+}
+#what
+{
+width:30%;
+height:20%;
+float:right;
+}
+#button
+{
+width:100%;
+height:4%;
+background:#20B2AA;
+}
+#foto
+{
+float:right;
+margin-right:0%;
+width:23%;
+height:54%;
+margin-top:2%;
+background color:white;
+background :url(img/tutu.jpeg);
+box-shadow:0px 0px 10px #C0C0C0;
+text-align:justify;
+} 
+#unique
+{
+color:#5F9EA0;
+float:right;
+margin:0% 2% 0% 0%;
+width:50%;
+height:relative;
+background color:white;
+margin-top:2%;
+box-shadow:0px 0px 10px #C0C0C0;
+box-align:justify;
+}
+#lilo
+{
+color:#5F9EA0;
+float:right;
+margin:0% 2% 30% 0%;
+width:50%;
+height:40%;
+background :#333333;
+border-radius:20px;
+box-shadow:0px 0px 5px #C0C0C0;
+box-align:justify;
+}
+#intel
+{
+float:left;
+margin-left:0% 0% 0% 0%;
+width:20%;
+margin-top:2%;
+height:relative%;
+background color:white;
+box-shadow:0px 0px 5px #C0C0C0;
+}
+
+</style>
 </head>
 <body>
-	<h3>welcome to collage automation syindexsytem</h3>
+<div id="container">
+<div id="header"><img src="<?php echo base_url('img/websiteLogo.png');?>">
+</div>
+<div id="what">
+<br/><br/><input type="text" name="abc" placeholder="what about you know" size="25">
+<input type="button" value="search" name="abc">
+</div>
+</div>
+<div id="button">
+<table width="100%" font size="30" align="center" style="background:none;">
+<th>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">Home</font></a></b></center>
+</td>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">About us</font></a></b></center>
+</td>
+<td><center><b><a href="registration.html" style="text-decoration:none;"><font color="white">Registration</font></a></b></center>
+</td>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">facilities</font></a></b></center>
+</td>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">Activities</font></a></b></center>
+</td>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">Gallery</font></a></b></center>
+</td>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">Contact us</font></a></b></center>
+</td>
+<td><center><b><a href="home.html" style="text-decoration:none;"><font color="white">News</font></a></b></center>
+</td>
+</th>
+</table>
+</div>
+<img src="img/ab.gif" width=100% height=40%>
+<div id="intel">
+<table>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">College documentory</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Prospectus</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Anti-ragging(circulars)</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Internal examination</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Library</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Arts</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Achievement in sports and games</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Staff club</li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Parent login</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Examination result</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Syllubus</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Site map</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Work with us</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Career guidance portal</font></li></font></a></b></td></tr>
+<tr><td><b><a href="home.html" style="text-decoration:none;"><font color=green><li><font color=#DB7093">Contact us</font></li></font></a></b></td></tr>
+</table>
+</div>
+<div id="foto">
+<br/><br/><br/>
+<br/><br/><center><input type="text" name="ccc" placeholder="Student name" size="25"></center>
+<br/><center><input type="text" name="ddd" placeholder="Student ID" size="25"></center>
+<br/><center><input type="button" name="vvv" value="SUBMIT"><a href="login.html"></a></button></center>
+</div>
+<br/>
+<div id="unique"><h1><center><p style="color:#2F4F4F;"><marquee behaviour="alternate" scrollamount="7" align="center">Welcome To Our College</marquee></p></center></h1>
+<h3>Jamia Nadwiyya Arts & Science College is a self financing college recognized by the Govt of Kerala and affilicated to the University of Calicut.It is run by the Jamia Nadwiyya Trust board, Edavanna.The College was established in 2003 with a main objective of uplifting the educationally back ward muslims other back ward communities in particular and all other communities in general,moulding them professionally competent,socially responsible and morally citizens.</h3>
+ </div>
+<br/><div id="lilo"><center><p class="style19 style3">Subcribe To Our Newsletter</p></center>
+ <form id="form1" name="form1" method="post" action="">
+                            <label><center>
+                              <input name="email" type="text" id="email" placeholder="Enter Email Here�" /><center>
+                              </label><br/>
+                            <label><center>
+                            <a href="home.html" style="text-decoration:none;"><img src="img/btn-subscribe.gif"></center>
+                            </label>
+                          </form>
+                          <p class="style19"><span class="style3">Follw Us </span> <img src="img/fl.png" width="27" height="26" align="absmiddle" /> <a href="Links/Facebook - Log In or Sign Up/Facebook - Log In or Sign Up.htm"><img src="img/fb.png" width="27" height="26" align="absmiddle" /></a> <a href="Links/Twitter - Login or Sign up/Welcome to Twitter - Login or Sign up.htm"><img src="img/tw.png" width="27" height="26" align="absmiddle" /></a> <a href="Links/Gmail - Free Storage and Email from Google/Gmail - Free Storage and Email from Google.htm"><img src="img/em.png" width="27" height="26" align="absmiddle" /></a></p>
+                         </center>
+                          <blockquote>
+                           <center> <p class="style19"><span class="copyright style20">Copyright � 2015 Dec</span></p></center>
+<center> <p class="style19"><span class="copyright style20">Developed & Designed by JESHMA NASRIN , NOUFAL CK , FAIZ MOHD</span></p></center></div>
+
 </body>
 </html>
+  
